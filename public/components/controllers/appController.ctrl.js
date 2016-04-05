c4ecc77b3ae15d96d4b5108f73bf17093dbc0c44
Jasmin Besic <jasko1991@live.com>
2016-04-05 23:37:20 +0200

    Splash screen prototype

--- public/components/controllers/appController.ctrl.js
+++ public/components/controllers/appController.ctrl.js
@@ -1,5 +1,11 @@
 angular.module('app')
 
-	.controller('mainController', ['$scope', function($scope) {
+	.controller('mainController', ['$scope', '$timeout', function($scope, $timeout) {
+/*
+		$timeout(function() {
+			jQuery("[data-splash]").fadeTo(100, 0, function() {
+				jQuery(this).remove();
+			});
+		}, 950);*/
 
 	}]);
