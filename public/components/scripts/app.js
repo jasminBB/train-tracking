4bcf86a1cd531a0bd496bcd34a17d94f557241c2
Jasmin Besic <jasko1991@live.com>
2016-04-06 21:18:52 +0200

    Markup for the splash screen as independent component

--- public/components/scripts/app.js
+++ public/components/scripts/app.js
@@ -1,1 +1,12 @@
-angular.module('app', ['ngMaterial']);
+angular.module('app', ['ngMaterial', 'ngRoute'])
+
+	.config(function($routeProvider) {
+
+		$routeProvider
+
+			.otherwise({
+				templateUrl: "components/views/initial-home.html",
+				redirectTo: "/"
+			});
+
+	});
